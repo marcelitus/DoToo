c20864137e96380177da7999cbe6ffba845e1d34
Marcelo Lanzarotti <4316762+marcelitus@users.noreply.github.com>
2020-10-15 13:00:30 -0500

    New Views and models

---
+++ DoToo/DoToo/ViewModels/ItemViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DoToo.Models;
+using DoToo.Repositories;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+
+namespace DoToo.ViewModels
+{
+    public class ItemViewClass : ViewModel
+    {
+        private readonly TodoItemRepository repository;
+        public ItemViewModel(TodoItemRepository repository)
+        {
+            this.repository = repository;
+        }
+    }
+
+}
---
+++ DoToo/DoToo/Views/MainView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using DoToo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DoToo.Views
+{
+    public partial class MainView : ContentPage
+    {
+        public MainView(MainView viewModel)
+        {
+            InitializeComponent();
+            viewModel.Navigation = Navigation;
+            BindingContext = viewModel;
+        }
+    }
+}
